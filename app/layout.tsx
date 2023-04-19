eeca87c184bc5f3fbb63d5436125d327299a313b
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2023-04-19 17:07:06 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import { Nunito } from "next/font/google";
+
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className={font.className}>
+        <ToasterProvider />
+        <LoginModal />
+        <RegisterModal />
+        <Navbar currentUser={currentUser} />
+        {children}
+      </body>
+    </html>
+  );
+}
