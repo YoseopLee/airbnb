241e4e152f91037a4027b74b7c708f3fb4e1f7b7
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2023-04-26 11:08:25 +0900

    fix/modal-type-err

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -6,8 +6,8 @@ import Button from "../Button";
 
 interface ModalProps {
   isOpen?: boolean;
-  onClose?: () => void;
-  onSubmit?: () => void;
+  onClose: () => void;
+  onSubmit: () => void;
   title?: string;
   body?: React.ReactElement;
   footer?: React.ReactElement;
@@ -44,7 +44,7 @@ const Modal: React.FC<ModalProps> = ({
     setTimeout(() => {
       onClose();
     }, 300);
-  }, [disabled, onClose]);
+  }, [onClose, disabled]);
 
   const handleSubmit = useCallback(() => {
     if (disabled) {
